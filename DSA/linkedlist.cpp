0a8cecfc5ca0292734beb5d8af9b7a934a267059
Kavin <kavinkj2004@gmail.com>
2024-06-01 21:28:58 +0530

    edit LinkedList

--- DSA/linkedlist.cpp
+++ DSA/linkedlist.cpp
@@ -1,43 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct node{
+struct node
+{
     int data;
-    node* next=nullptr;
+    node* next=NULL;
 };
 
-void addnode(int data,node* &head){
-        node* newnode = new node();
-        newnode->data = data;
-        newnode->next = head;
-        head = newnode;
+
+void addnode(int a,node* &head){
+    node* newnode = new node();
+    newnode->data = a;
+    newnode->next = head;
+    head = newnode;
 }
 
-void display(node* &temp){
+void display(node* &head){
+    node* temp = head;
     while(temp!=NULL){
         cout<<temp->data<<" ";
         temp = temp->next;
     }
     cout<<"\n";
-    return;
+
 }
 
-
 int main(){
-    node *head=nullptr;
+    node* head=NULL;
     int n;
-    cout<<"No of elements : ";
     cin>>n;
+    int a;
     for(int i=0;i<n;i++){
-        int a;
         cin>>a;
         addnode(a,head);
     }
-    try{
-        display(head);
-    }
-    catch(string e){
-        cout<<e;
-    }
-    return 0;
+    display(head);
 }
+
+
+
+
