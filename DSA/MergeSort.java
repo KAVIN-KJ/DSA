cde572fc0890fcdbf6d4154e84246f789e3c5bfa
Kavin <kavinkj2004@gmail.com>
2024-06-22 16:40:12 +0530

    Day 2

--- DSA/MergeSort.java
+++ DSA/MergeSort.java
@@ -8,46 +8,51 @@ public class MergeSort {
     }
 
     private static void mergeSort(int[] arr) {
+        
         if(arr.length<=1) return;
-        int ltsize = arr.length/2;
-        int rtsize = arr.length - ltsize;
-        int lt[] = new int[ltsize];
-        int rt[] = new int[rtsize];
+        int length = arr.length;
         int j=0;
-        for(int i=0;i<arr.length;i++){
-            if(i<ltsize){
+        int lts=length/2; 
+        int lt[] = new int[lts];
+        int rt[] = new int[length-lts];
+        for(int i=0;i<length;i++){
+            if(i<lts){
                 lt[i] = arr[i];
-            }
-            else{
+            }else{
                 rt[j] = arr[i];
                 j++;
             }
         }
         mergeSort(lt);
         mergeSort(rt);
-        merge(lt,rt,arr);
+        merge(lt, rt, arr);
+
     }
 
     private static void merge(int[] lt, int[] rt, int[] arr) {
+
+        int lts = lt.length;
+        int rts = rt.length;
         int i=0,l=0,r=0;
-        while(l<lt.length && r<rt.length){
-            if(lt[l]>rt[r]){
-                arr[i] = rt[r];
-                r++;
-                i++;
-            }
-            else{
+        while(l<lts && r<rts){
+            if(lt[l] < rt[r]){
                 arr[i] = lt[l];
                 l++;
                 i++;
             }
+            else{
+                arr[i] = lt[l];
+                arr[i] = rt[r];
+                r++;
+                i++;
+            }
         }
-        while(l<lt.length){
+        while(l<lts){
             arr[i] = lt[l];
             l++;
             i++;
         }
-        while(r<rt.length){
+        while(r<rts){
             arr[i] = rt[r];
             r++;
             i++;
