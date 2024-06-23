50580ba7307022314de69269c840f7c39a18b971
Kavin <kavinkj2004@gmail.com>
2024-06-23 21:52:48 +0530

    Day 4

--- DSA/QuickSort.java
+++ DSA/QuickSort.java
@@ -1,33 +1,35 @@
 public class QuickSort {
-    public static void main(String[] args) {
-        int arr[] = {8,4,5,3,9,7,6,1,10,2};
-        quickSort(arr,0,arr.length-1);   
-        for(int i : arr){
-            System.out.print(i+" ");
+public static void main(String[] args) {
+    int arr[] = {10,9,1,8,2,7,3,6,4,5};
+    quickSort(arr,0,arr.length-1);
+    for(int i : arr){
+        System.out.print(i+" ");
+    }
+}
+
+private static void quickSort(int[] arr, int st, int nd) {
+    if(nd<=st) return;
+    int pivot = partition(arr,st,nd);
+    quickSort(arr, st, pivot-1);
+    quickSort(arr, pivot+1,nd);
+}
+
+private static int partition(int[] arr, int st, int nd) {
+    int i=st-1;
+    int piv = nd;
+    for(int j=st;j<nd;j++){
+        if(arr[j] < arr[piv]){
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
         }
     }
-
-    private static void quickSort(int[] arr, int st, int nd) {
-        if(nd<=st) return;
-        int pivot = partition(arr,st,nd);
-        quickSort(arr, st, pivot-1);
-        quickSort(arr, pivot+1, nd);
-    }
-
-    private static int partition(int[] arr, int st, int nd) {
-        int i=st-1;
-        for(int j=st;j<nd;j++){
-            if(arr[j] < arr[nd]){
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[nd];
-        arr[nd] = temp;
-        return i;
-    }
+    i++;
+    int temp = arr[i];
+    arr[i] = arr[piv];
+    arr[piv] = temp;
+    return i;
+}
+    
 }
