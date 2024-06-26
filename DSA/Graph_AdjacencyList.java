cf38a6301129ba8bbf6059585bd44e7c0d30af0b
Kavin <kavinkj2004@gmail.com>
2024-06-26 22:29:00 +0530

    Day 5

--- DSA/Graph_AdjacencyList.java
+++ DSA/Graph_AdjacencyList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Node{
     char data;
     Node(char dat){
@@ -7,11 +9,54 @@ class Node{
 
 
 class Graph{
-    ArrayList<Node> nodes;
+    static ArrayList<ArrayList<Node>> nodes;
+    Graph(){
+        nodes = new ArrayList<>();
+    }
+    public void addNode(char dat){
+        ArrayList<Node> curr = new ArrayList<>();
+        curr.add(new Node(dat));
+        nodes.add(curr);
+    }
+    public void addEdge(int src,int dest){
+        nodes.get(src).add(nodes.get(dest).get(0));
+    }
+
+    public void printGraph(){
+        for(int i=0;i<nodes.size();i++){
+            for(int j=0;j<nodes.get(i).size();j++){
+                System.out.print(nodes.get(i).get(j).data);
+                if(j<nodes.get(i).size()-1){
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
 
 
 public class Graph_AdjacencyList {
-    
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addNode('A');
+        graph.addNode('B');
+        graph.addNode('C');
+        graph.addNode('D');
+        graph.addNode('E');
+        graph.addEdge(0,1);
+        graph.addEdge(0,3);
+        graph.addEdge(0,4);
+        graph.addEdge(1,2);
+        graph.addEdge(1,3);
+        graph.addEdge(1,4);
+        graph.addEdge(2,4);
+        graph.addEdge(3,0);
+        graph.addEdge(3,1);
+        graph.addEdge(3,4);
+        graph.addEdge(4,3);
+        graph.printGraph();
+    }
+   
 }
