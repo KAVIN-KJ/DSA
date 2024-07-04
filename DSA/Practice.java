b59365f5dbf5b4d92db73db8ea7c0a2446dec937
Kavin <kavinkj2004@gmail.com>
2024-07-04 22:29:22 +0530

    Day 11

--- DSA/Practice.java
+++ DSA/Practice.java
@@ -1,3 +1,5 @@
+
+import java.util.*;
 //  MERGE SORT
 
 // public class Practice {
@@ -56,40 +58,117 @@
 
 // PRINT SUBSEQUENCES
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
+
+// public class Practice {
+     
+//     public static void main(String[] args) {
+//         int arr[] = {1,5,2,6,4,8,9,10};
+//         ArrayList<Integer> al = new ArrayList<>();
+//         printSubseq(0,arr,al);
+//         int k = 15;
+//         printSubseq_With_SumK(0,arr,al,k,0);
+//     }
+
+//     private static void printSubseq_With_SumK(int i,int[] arr, ArrayList<Integer> al, int k,int curr) {
+//          if(i>=arr.length){
+//             if(curr==k){
+//                 System.out.println(al);
+//             }
+//             return;
+//          }
+//          al.add(arr[i]);
+//          printSubseq_With_SumK(i+1, arr, al, k, curr+arr[i]);
+//          al.remove(al.getLast());
+//          printSubseq_With_SumK(i+1, arr, al, k, curr);
+//      }
+
+//     private static void printSubseq(int i, int[] arr, ArrayList<Integer> al) {
+//         if(i>=arr.length){
+//             System.out.println(al);
+//             return;
+//         }
+//         al.add(arr[i]);
+//         printSubseq(i+1, arr, al);
+//         al.remove(al.getLast());
+//         printSubseq(i+1, arr, al);
+//     }
+    
+// }
+
+
+// TRAINING NINJA
+
+
+// public class Practice {
+
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         int days = in.nextInt();
+//         int task = in.nextInt();
+//         int tasks[][] = new int[days][task];
+//         for(int i=0;i<days;i++){
+//             for(int j=0;j<task;j++){
+//                 tasks[i][j] = in.nextInt();
+//             }
+//         }
+//         int dp[][] = new int[days][task];
+//         for(int i=0;i<days;i++){
+//             for(int j=0;j<task;j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         int ans = trainNinja(days-1,-1,tasks,dp);
+//         System.out.println(ans);
+//     }
+//     private static int trainNinja(int day, int last, int[][] tasks, int[][] dp) {
+//         if(day==0){
+//             int max=0;
+//             for(int i=0;i<tasks[day].length;i++){
+//                 if(i!=last)
+//                 max = Math.max(max,tasks[day][i]);
+//             }
+//             return max;
+//         }
+//         if(last!=-1&&dp[day][last]!=-1) return dp[day][last];
+//         int max=0;
+//         for(int i=0;i<tasks[day].length;i++){
+//             if(i!=last){
+//                 int points = tasks[day][i] + trainNinja(day-1, i, tasks, dp);
+//                 max = Math.max(points,max);
+//             }
+//         }
+//         if(last!=-1) dp[day][last] = max;
+//         return max;
+//     }   
+// }
+
+//  INSERTION SORT
 
 public class Practice {
-     
+
     public static void main(String[] args) {
-        int arr[] = {1,5,2,6,4,8,9,10};
-        ArrayList<Integer> al = new ArrayList<>();
-        printSubseq(0,arr,al);
-        int k = 15;
-        printSubseq_With_SumK(0,arr,al,k,0);
-    }
-
-    private static void printSubseq_With_SumK(int i,int[] arr, ArrayList<Integer> al, int k,int curr) {
-         if(i>=arr.length){
-            if(curr==k){
-                System.out.println(al);
-            }
-            return;
-         }
-         al.add(arr[i]);
-         printSubseq_With_SumK(i+1, arr, al, k, curr+arr[i]);
-         al.remove(al.getLast());
-         printSubseq_With_SumK(i+1, arr, al, k, curr);
-     }
-
-    private static void printSubseq(int i, int[] arr, ArrayList<Integer> al) {
-        if(i>=arr.length){
-            System.out.println(al);
-            return;
+        int arr[] = {9,1,4,2,3,7,6,8,0,5};
+        insertSort(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
         }
-        al.add(arr[i]);
-        printSubseq(i+1, arr, al);
-        al.remove(al.getLast());
-        printSubseq(i+1, arr, al);
     }
+
+    private static void insertSort(int[] arr) {
+        int n = arr.length;
+        for(int i=1;i<n;i++){ 
+            int j = i-1;
+            int temp = arr[i];
+            while(j>=0&&arr[j]>temp){ 
+                arr[j+1]=arr[j];
+                j--; 
+            }
+            arr[j+1]=temp;       
+         }
+    }
+
     
 }
+
+
--- DSA/TrainingNinja.java
+++ DSA/TrainingNinja.java
@@ -22,11 +22,42 @@ public class TrainingNinja {
         }
         int a = maxCredits(days-1,3,arr);
         int b = maxCredits(days-1,-1, arr, dp);
+        int c = maxCredits(arr);
         in.close(); 
         System.out.println("Max Credits : "+a);
-        System.out.println("Max Credits : "+b);
+        System.out.println("Max Credits Memoization : "+b);
+        System.out.println("Max Credits Tabulation : "+c);
     }
-// Recursive
+// TABULATED
+private static int maxCredits(int[][] arr) {
+        int days = arr.length;
+        int tasks = arr[0].length;
+        int dp[][] = new int[days][tasks];
+        dp[0][0] = Math.max(arr[0][1],arr[0][2]);
+        dp[0][1] = Math.max(arr[0][0],arr[0][2]);
+        dp[0][2] = Math.max(arr[0][1],arr[0][0]);
+        int ans=0;
+        for(int day=1;day<days;day++){
+            for(int last=0;last<tasks;last++){
+                dp[day][last] = 0;
+                for(int task=0;task<tasks;task++){
+                    if(task!=last){
+                        int point = arr[day][task] + dp[day-1][last];
+                        dp[day][last] = Math.max(point,dp[day][last]);
+                        ans = dp[day][last];
+                    }
+                }
+            }
+        }
+        for(int[] i : dp){
+            for(int j : i){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        return ans;
+}
+    // Recursive
     private static int maxCredits(int day, int last, int[][] arr) {
         if(day==0){
             int maxPt = 0;
@@ -64,10 +95,7 @@ private static int maxCredits(int day, int last, int[][] arr,int[][] dp) {
             maxPt = Math.max(point,maxPt);
         }
     }
-    if(last!=-1){
-        dp[day][last] = maxPt;
-    }
+    if(last!=-1) dp[day][last] = maxPt;
     return maxPt;
 }
-
 }
