818dc51b9b9479def1330cabe1b8c231e84c8f9d
KJKR <kavinkj2004@gmail.com>
2024-08-17 10:22:50 +0530

    Day 22

--- Dynamic Programming/MergeSort.java
+++ Dynamic Programming/MergeSort.java
@@ -15,58 +15,6 @@ public class MergeSort {
 
     }
 
-    // private static void mergeSort(int[] arr) {
-
-    // if(arr.length<=1) return;
-    // int length = arr.length;
-    // int j=0;
-    // int lts=length/2;
-    // int lt[] = new int[lts];
-    // int rt[] = new int[length-lts];
-    // for(int i=0;i<length;i++){
-    // if(i<lts){
-    // lt[i] = arr[i];
-    // }else{
-    // rt[j] = arr[i];
-    // j++;
-    // }
-    // }
-    // mergeSort(lt);
-    // mergeSort(rt);
-    // merge(lt, rt, arr);
-
-    // }
-
-    // private static void merge(int[] lt, int[] rt, int[] arr) {
-
-    // int lts = lt.length;
-    // int rts = rt.length;
-    // int i=0,l=0,r=0;
-    // while(l<lts && r<rts){
-    // if(lt[l] < rt[r]){
-    // arr[i] = lt[l];
-    // l++;
-    // i++;
-    // }
-    // else{
-    // arr[i] = lt[l];
-    // arr[i] = rt[r];
-    // r++;
-    // i++;
-    // }
-    // }
-    // while(l<lts){
-    // arr[i] = lt[l];
-    // l++;
-    // i++;
-    // }
-    // while(r<rts){
-    // arr[i] = rt[r];
-    // r++;
-    // i++;
-    // }
-    // }
-
     private static void mergeSort(int[] arr) {
         if (arr.length <= 1)
             return;
