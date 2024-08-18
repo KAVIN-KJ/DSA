5362dc68fd678df245f079f44a17aeecb18b7075
KJKR <kavinkj2004@gmail.com>
2024-08-18 21:35:20 +0530

    Day 23

--- Dynamic Programming/CanPartitionWithEqualSum.java
+++ Dynamic Programming/CanPartitionWithEqualSum.java
@@ -1,43 +1,46 @@
 import java.util.Scanner;
 
+// TEST : 2 3 3 3 4 5 SUM = 20, ANSWER = TRUE
 public class CanPartitionWithEqualSum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
         int target = in.nextInt();
-        boolean dp[][] = new boolean[n][target+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=target;j++){
+        boolean dp[][] = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= target; j++) {
                 dp[i][j] = false;
             }
         }
+        System.out.println(solve(arr));
         in.close();
     }
 
-    public static boolean solve(int[] arr){
+    public static boolean solve(int[] arr) {
         int n = arr.length;
         int sum = 0;
-        for(int i : arr) sum+=i;
-        if(sum%2==1) return false;
+        for(int i : arr){
+            sum+=i;
+        }
+        if (sum%2==1) return false;
         int trgt = sum/2;
         boolean dp[][] = new boolean[n][trgt+1];
         for(int i=0;i<n;i++){
-            dp[i][0]=true;
+            dp[i][0] = true;
         }
         if(arr[0]<=trgt) dp[0][arr[0]] = true;
-        for(int ind = 1;ind<n;ind++){
-            for(int tgt=1;tgt<=trgt;tgt++){
-                boolean ntake = dp[ind-1][trgt];
-                boolean take=false;
-                if(arr[ind]<=tgt) take = dp[ind-1][tgt-arr[ind]];
-                dp[ind][tgt] = ntake||take;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=trgt;j++){
+                boolean take = false;
+                boolean n_take = dp[i-1][j];
+                if(arr[i]<=j) take = dp[i-1][j-arr[i]];
+                dp[i][j] = take|n_take;
             }
         }
-
         return dp[n-1][trgt];
     }
 
--- Dynamic Programming/HasSubsequenceWithTargetSum.java
+++ Dynamic Programming/HasSubsequenceWithTargetSum.java
@@ -56,20 +56,38 @@ public class HasSubsequenceWithTargetSum {
     }
 
 // TABULATED
-    public static boolean hasSubseq(int[] arr, int trgt){
+    // public static boolean hasSubseq(int[] arr, int trgt){
+    //     int n = arr.length;
+    //     boolean dp[][] = new boolean[n][trgt+1];
+    //     for(int i=0;i<n;i++){
+    //         dp[i][0] = true;
+    //     }
+    //     if(arr[0]<=trgt) dp[0][arr[0]] = true;
+
+    //     for(int i=1;i<n;i++){
+    //         for(int j=1;j<=trgt;j++){
+    //             boolean n_take = dp[i-1][trgt];
+    //             boolean take = false;
+    //             if(arr[i]<=j) take = dp[i-1][j-arr[i]];
+    //             dp[i][j] = n_take||take;
+    //         }
+    //     }
+    //     return dp[n-1][trgt];
+    // }
+
+    public static boolean hasSubseq(int arr[], int trgt){
         int n = arr.length;
         boolean dp[][] = new boolean[n][trgt+1];
         for(int i=0;i<n;i++){
             dp[i][0] = true;
         }
-        if(arr[0]<=trgt) dp[0][arr[0]] = true;
-
+        if(arr[0] <= trgt) dp[0][arr[0]] = true;
         for(int i=1;i<n;i++){
             for(int j=1;j<=trgt;j++){
-                boolean n_take = dp[i-1][trgt];
                 boolean take = false;
+                boolean n_take = dp[i-1][j];
                 if(arr[i]<=j) take = dp[i-1][j-arr[i]];
-                dp[i][j] = n_take||take;
+                dp[i][j] = take||n_take;
             }
         }
         return dp[n-1][trgt];
