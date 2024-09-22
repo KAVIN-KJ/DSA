1d33e21389cd121b1232e733a1477c0ba015e8a1
KJKR <kavinkj2004@gmail.com>
2024-09-22 23:06:46 +0530

    Day 34

---
+++ Dynamic Programming/MinimumInsertionsForPalindrome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class MinimumInsertionsForPalindrome {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        System.out.println(minInsertions(s));
+    }
+    public static int minInsertions(String s) {
+        int ans = lps(s);
+        return s.length()-ans;
+    }
+    public static int lps(String s){
+        int n = s.length();
+        String r = new StringBuilder(s).reverse().toString();
+        int dp[][] = new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+            dp[0][i] = 0;
+        } 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==r.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][n];
+    }
+}
