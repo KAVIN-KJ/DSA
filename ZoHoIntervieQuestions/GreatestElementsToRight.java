ffab790b32b2c5b59351c8c4b646b63bb20b6bb8
KJKR <kavinkj2004@gmail.com>
2024-12-13 20:37:05 +0530

    Day 39

---
+++ ZoHoIntervieQuestions/GreatestElementsToRight.java
@@ -0,0 +1,24 @@
+package ZoHoIntervieQuestions;
+import java.util.*;
+public class GreatestElementsToRight {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        // iterate from last - maximum so far towards the right is = -1
+        // calculate newMaximum from comparing current element and right max
+        // set current element to rightMax;
+        // set rightMax to newMax
+        int rightMax = -1;
+        for(int i=arr.length-1;i>=0;i--){
+            int newMax = Math.max(arr[i],rightMax);
+            arr[i] = rightMax;
+            rightMax = newMax;
+        }
+        for(int i : arr)
+            System.out.print(i+" ");
+    }
+}
