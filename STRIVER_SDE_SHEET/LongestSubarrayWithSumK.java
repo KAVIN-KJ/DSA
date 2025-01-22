906acfd1adcc168c578d64070ce02ccf829d8b83
KJKR <kavinkj2004@gmail.com>
2025-01-22 23:55:15 +0530

    Day 60

---
+++ STRIVER_SDE_SHEET/LongestSubarrayWithSumK.java
@@ -0,0 +1,50 @@
+package STRIVER_SDE_SHEET;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LongestSubarrayWithSumK {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int k = in.nextInt();
+        int len = lengthOfLongest(arr, k);
+        System.out.println(len);
+        len = countSubArrays(arr, k);
+        System.out.println(len);
+        in.close();
+    }
+
+    private static int lengthOfLongest(int[] arr, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (mp.getOrDefault(sum - k, -1) != -1) {
+                len = Math.max(len, i - mp.get(sum - k));
+            }
+            if (!mp.containsKey(sum))
+                mp.put(sum, i);
+        }
+        return len;
+    }
+    public static int countSubArrays(int arr[],int k){
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        mp.put(0,1);
+        int sum = 0;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(mp.containsKey(sum-k)){
+                count+=mp.get(sum-k);
+            }
+            mp.put(sum, mp.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
