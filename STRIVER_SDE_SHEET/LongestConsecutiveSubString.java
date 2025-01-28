dc8ac16b95f0bea0c24cb2bcc7345433d4b01e57
KJKR <kavinkj2004@gmail.com>
2025-01-28 23:16:01 +0530

    Day 65

---
+++ STRIVER_SDE_SHEET/LongestConsecutiveSubString.java
@@ -0,0 +1,34 @@
+package STRIVER_SDE_SHEET;
+
+import java.util.*;
+
+public class LongestConsecutiveSubString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int ans = longestConsecSubstr(s);
+        System.out.println(ans);
+        in.close();
+    }
+
+    private static int longestConsecSubstr(String s) {
+        int ans = 0;
+        int hash[] = new int[256];
+        Arrays.fill(hash,-1);
+        int l = 0;
+        int r = 0;
+        while(r<s.length()){
+            int x = s.charAt(r);
+            if(hash[x]!=-1){
+                if(hash[x]>=l){
+                    l = hash[x]+1;
+                }
+            }
+            hash[x] = r;
+            ans = Math.max(ans,(r-l)+1);
+            r++;
+        }
+        
+        return 0;
+    }
+}
