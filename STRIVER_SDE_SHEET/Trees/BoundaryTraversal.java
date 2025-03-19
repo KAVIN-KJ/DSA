abe570e330b2264f1f9b6d526c84bf275e231dfc
KAVIN-KJ <kavinkj2004@gmail.com>
2025-03-19 23:04:42 +0530

    Day 98

---
+++ STRIVER_SDE_SHEET/Trees/BoundaryTraversal.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BoundaryTraversal {
+    void leftBound(Node root,ArrayList<Integer> res){
+        if(root==null) return;
+        Node temp = root.left;
+        while(temp!=null){
+            if(temp.left!=null || temp.right!=null) res.add(temp.data);
+            if(temp.left!=null) temp=temp.left;
+            else temp = temp.right;
+        }
+        
+    }
+    
+    void rightBound(Node root,ArrayList<Integer> res){
+        if(root==null) return;
+        ArrayList<Integer> temp = new ArrayList<>();
+        Node tp = root.right;
+        while(tp!=null){
+            if(tp.left!=null || tp.right!=null) temp.add(tp.data);
+            if(tp.right!=null) tp = tp.right;
+            else tp = tp.left;
+        }
+        Collections.reverse(temp);
+        res.addAll(temp);
+    }
+    
+    void leafNodes(Node root,ArrayList<Integer> res){
+        if(root==null) return;
+        if(root!=null && root.left==null && root.right==null){
+            res.add(root.data);
+            return;
+        }
+        leafNodes(root.left,res);
+        leafNodes(root.right,res);
+    }
+    
+    
+    
+    ArrayList<Integer> boundaryTraversal(Node node) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        if(node==null) return al;
+        if(node.left==null && node.right==null){
+            al.add(node.data);
+            return al;
+        }
+        al.add(node.data);
+        leftBound(node,al);
+        leafNodes(node,al);
+        rightBound(node,al);
+        return al;
+    }
+}
---
+++ STRIVER_SDE_SHEET/Trees/ZigZagTraversal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ZigZagTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean ltr = true;
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode cur = q.poll();
+                if(ltr){
+                    level.add(cur.val);
+                }
+                else level.addFirst(cur.val);
+                if(cur.left!=null) q.add(cur.left);
+                if(cur.right!=null) q.add(cur.right);
+            }
+            ltr=!ltr;
+            ans.add(level);
+        }
+        return ans;
+    }
+}
