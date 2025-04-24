b43fe5b858fb9dee64637b593baabb0fb3b87923
KAVIN-KJ <kavinkj2004@gmail.com>
2025-04-24 20:42:05 +0530

    Revision | Just to get in touch

--- Practice/Random_Revision/LCA_BST.java
+++ Practice/Random_Revision/LCA_BST.java
@@ -1,13 +1,10 @@
 package Random_Revision;
-
-
-public class LCA_BST {
-    public static TreeNode lca(TreeNode root,TreeNode p,TreeNode q){
-        if(root==null || root==p || root==q){
-            return root;
-        }
-        TreeNode lt = lca(root.left,p,q);
-        TreeNode rt = lca(root.right,p,q);
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return root;
+        if(root==p||root==q) return root;
+        TreeNode lt = lowestCommonAncestor(root.left,p,q);
+        TreeNode rt = lowestCommonAncestor(root.right,p,q);
         if(lt!=null && rt!=null) return root;
         return lt!=null ? lt : rt;
     }
