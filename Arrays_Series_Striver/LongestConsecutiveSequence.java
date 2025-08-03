55a51af78a3a0cef5df24c97a8961cd88c423205
KAVIN-KJ <kavinkj2004@gmail.com>
2025-08-03 22:23:07 +0530

    Day 32

---
+++ Arrays_Series_Striver/LongestConsecutiveSequence.java
@@ -0,0 +1,24 @@
+package Arrays_Series_Striver;
+
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+    public int solution(int arr[]){
+        HashSet<Integer> st = new HashSet<>();
+        if(arr.length==0) return 0;
+        int ans = 1;
+        for(int i : arr) st.add(i);
+        
+        for(int i : st){
+            if(!st.contains(i-1)){
+                int cur = i;
+                int len = 0;
+                while(st.contains(cur++)){
+                    len++;
+                }
+                ans = Math.max(len,ans);
+            }
+        }
+        return ans;
+    }
+}
