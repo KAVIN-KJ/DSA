af7099db1b1ab00d60229ed30fa208cc903a8871
KAVIN-KJ <kavinkj2004@gmail.com>
2025-08-04 23:04:33 +0530

    Day 33

---
+++ Arrays_Series_Striver/CountSubarraysWithXorK.java
@@ -0,0 +1,17 @@
+package Arrays_Series_Striver;
+
+import java.util.HashMap;
+
+public class CountSubarraysWithXorK {
+    public long subarrayXor(int arr[], int k) {
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int xor = 0,ans = 0;
+        for(int i : arr){
+            xor ^= i;
+            if(xor==k) ans++;
+            if(mpp.containsKey(xor^k)) ans+=mpp.get(xor^k);
+            mpp.put(xor,mpp.getOrDefault(xor,0)+1);
+        }
+        return ans;
+    }
+}
